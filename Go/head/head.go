3299ee4e686ec981db6554f2f152611601961637
Andrew Morris <andrew@morris.sc>
2017-04-02 09:35:40 -0400

    formatting

--- Go/head/head.go
+++ Go/head/head.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func headFile(nameOfFileToRead string) {
-    data, err := ioutil.ReadFile(nameOfFileToRead)
-    check(err)
-    s := string(data)
-    slicedFile := strings.Split(s, "\n")
-    firstTen := slicedFile[0:10]
-    for _, x := range firstTen {
-        fmt.Println(x)
-    }
+	data, err := ioutil.ReadFile(nameOfFileToRead)
+	check(err)
+	s := string(data)
+	slicedFile := strings.Split(s, "\n")
+	firstTen := slicedFile[0:10]
+	for _, x := range firstTen {
+		fmt.Println(x)
+	}
 }
 
 func main() {
-    if len(os.Args) == 2 {
-        argFilename := os.Args[1]
-        headFile(argFilename)
-    } else {
-        fmt.Println("head <filename>")
-        fmt.Println("head /etc/passwd")
-    }
+	if len(os.Args) == 2 {
+		argFilename := os.Args[1]
+		headFile(argFilename)
+	} else {
+		fmt.Println("head <filename>")
+		fmt.Println("head /etc/passwd")
+	}
 }
--- Go/tail/tail.go
+++ Go/tail/tail.go
@@ -1,35 +1,35 @@
 package main
 
 import (
-    "fmt"
-    "io/ioutil"
-    "os"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func tailFile(nameOfFileToRead string) {
-    data, err := ioutil.ReadFile(nameOfFileToRead)
-    check(err)
-    s := string(data)
-    slicedFile := strings.Split(s, "\n")
-    lastTen := slicedFile[len(slicedFile)-10:]
-    for _, x := range lastTen {
-        fmt.Println(x)
-    }
+	data, err := ioutil.ReadFile(nameOfFileToRead)
+	check(err)
+	s := string(data)
+	slicedFile := strings.Split(s, "\n")
+	lastTen := slicedFile[len(slicedFile)-10:]
+	for _, x := range lastTen {
+		fmt.Println(x)
+	}
 }
 
 func main() {
-    if len(os.Args) == 2 {
-        argFilename := os.Args[1]
-        tailFile(argFilename)
-    } else {
-        fmt.Println("tail <filename>")
-        fmt.Println("tail /etc/passwd")
-    }
+	if len(os.Args) == 2 {
+		argFilename := os.Args[1]
+		tailFile(argFilename)
+	} else {
+		fmt.Println("tail <filename>")
+		fmt.Println("tail /etc/passwd")
+	}
 }
