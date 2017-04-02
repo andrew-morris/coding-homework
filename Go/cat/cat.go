6259731ccce14c8bd4ec77e52f4692dd3f35847c
Andrew Morris <andrew@morris.sc>
2017-04-02 09:15:00 -0400

    formatting

--- Go/cat/cat.go
+++ Go/cat/cat.go
@@ -8,20 +8,20 @@ import "io/ioutil"
 //TOOD: Less disgusting errors
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func readFile(nameOfFileToRead string) {
-    data, err := ioutil.ReadFile(nameOfFileToRead)
-    check(err)
-    fmt.Print(string(data))
+	data, err := ioutil.ReadFile(nameOfFileToRead)
+	check(err)
+	fmt.Print(string(data))
 }
 
 func main() {
 	if len(os.Args) == 2 {
 		argString := os.Args[1]
-        readFile(argString)
+		readFile(argString)
 	}
 }
