1f5f3d48a7df6596298c415ee6a1a39f77e20d3e
Andrew Morris <andrew@morris.sc>
2017-04-02 09:14:46 -0400

    formatting

--- Go/grep/grep.go
+++ Go/grep/grep.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-    "fmt"
-    "os"
-	"strings"
+	"fmt"
 	"io/ioutil"
+	"os"
+	"strings"
 )
 
 //TODO: Implement proper usage
@@ -14,30 +14,30 @@ import (
 //TODO: Add -v (ignore) option
 
 func check(e error) {
-    if e != nil {
-        panic(e)
-    }
+	if e != nil {
+		panic(e)
+	}
 }
 
 func grepFile(nameOfFileToRead string, keywordToLookFor string) {
-    data, err := ioutil.ReadFile(nameOfFileToRead)
-    check(err)
+	data, err := ioutil.ReadFile(nameOfFileToRead)
+	check(err)
 	s := string(data)
 	slicedFile := strings.Split(s, "\n")
 	for _, x := range slicedFile {
 		if strings.Contains(x, keywordToLookFor) {
-            fmt.Println(x)
-        }
+			fmt.Println(x)
+		}
 	}
 }
 
 func main() {
-    if len(os.Args) == 3 {
-        argFilename := os.Args[2]
-        argKeyword := os.Args[1]
-        grepFile(argFilename, argKeyword)
-    } else {
-        fmt.Println("grep <keyword> <filename>")
-        fmt.Println("grep /etc/passwd root")
-    }
+	if len(os.Args) == 3 {
+		argFilename := os.Args[2]
+		argKeyword := os.Args[1]
+		grepFile(argFilename, argKeyword)
+	} else {
+		fmt.Println("grep <keyword> <filename>")
+		fmt.Println("grep /etc/passwd root")
+	}
 }
